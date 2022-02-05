940ef869cd56a579281b77dd353b2269c4be9540
razdu <razdusm@gmail.com>
2022-02-05 22:25:47 +0200

    last changes

--- models/movie.js
+++ models/movie.js
@@ -1,15 +1,15 @@
 const mongoose = require("mongoose");
 const { stringify } = require("querystring");
 const movieSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  publishDate: {
-    type: Date,
-    required: true,
-  },
-  cast: [],
-  originCountry: String,
+    name: {
+        type: String,
+        required: true
+    },
+    publishDate: {
+        type: Date,
+        required: true
+    },
+    cast: [],
+    originCountry: { type: String, index: true }
 });
 module.exports = mongoose.model("Movie", movieSchema);
